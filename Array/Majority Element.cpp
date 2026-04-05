8bdf68395302d0cf7a5988fd46c782342f8dc384
jinay378 <jinay.s5@ahduni.edu.in>
2026-04-05 09:34:04 +0530

    Search a 2D Matrix 2

---
+++ Array/Majority Element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) 
+    {
+        int n=nums.size();
+        for(int val:nums)
+        {
+            int freq=0;
+            for(int ele:nums)
+            {
+                if(ele==val)
+                {
+                    freq++;
+                }
+            }
+            if(freq>n/2)
+            {
+                return val;
+            }
+        }
+        return 0;
+    }    
+};
